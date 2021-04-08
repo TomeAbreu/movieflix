8fa18b73f6cf58fc51f7cf121499f5fa2dd4e72f
Rui Abreu <rui.abreu@impactzero.pt>
2021-04-08 15:19:23 +0100

    Fist Commit of MovieFlix APP with Firebase config already made

---
+++ config/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAo6bL0LdUN_rX07yH5ajsh6s0pjfnRdok",
+  authDomain: "movieflix-1a0c0.firebaseapp.com",
+  projectId: "movieflix-1a0c0",
+  storageBucket: "movieflix-1a0c0.appspot.com",
+  messagingSenderId: "419494860861",
+  appId: "1:419494860861:web:175ca89cda0e89011c3254",
+};
