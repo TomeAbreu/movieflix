7197369da9628be784080a2b9f0b099cdd42274a
Rui Abreu <rui.abreu@impactzero.pt>
2021-04-09 11:51:46 +0100

    Added navbar with transition animations and logos

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NavBar.css";
 
 const NavBar = () => {
-  return <div className="nav"></div>;
+  const [show, setShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_content">
+        <img className="nav_logo" src="/tmdb.new.logo.svg.png" alt="" />
+        <img className="nav_avatar" src="/avatar-tmdb.png" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
